b89138b4c1d0b46309b69b4a8b911eb6ec21299f
黄龑枫 <huangyanfeng@huangyanfengs-MacBook-Air.local>
2025-05-14 15:15:50 +0800

    修改vite.config文件

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,12 @@
 // vite.config.js
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueI18n from '@intlify/vite-plugin-vue-i18n'
 import path from 'path'
 
 export default defineConfig({
   base: './',                // 确保生产环境下资源引用相对路径
   plugins: [
-    vue(),
-    vueI18n({
-      // 指向你所有语言文件的文件夹
-      include: path.resolve(__dirname, './src/i18n/**')
-    })
+    vue()
   ],
   resolve: {
     alias: {
